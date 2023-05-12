88ab712fa82d9e6ce203549b91e6302f24fcaf4c
Luca <73708474+Luca-Landri@users.noreply.github.com>
2023-05-12 12:26:40 +0200

    Feat: Color is a variable

--- lib/components/button.dart
+++ lib/components/button.dart
@@ -2,8 +2,12 @@ import 'package:flutter/material.dart';
 
 class Button extends StatefulWidget {
   final String label;
+  final Color color;
   // ignore: prefer_const_constructors_in_immutables
-  Button({required this.label});
+  Button({
+    required this.label,
+    required this.color,
+  });
 
   @override
   State<Button> createState() => _ButtonState();
@@ -11,26 +15,31 @@ class Button extends StatefulWidget {
 
 class _ButtonState extends State<Button> {
   late String label = widget.label;
+  late Color color = widget.color;
 
   @override
   Widget build(BuildContext context) {
-    return ElevatedButton(
-      style: ButtonStyle(
-        backgroundColor: MaterialStateProperty.all<Color>(Colors.orange),
-        minimumSize: MaterialStateProperty.all<Size>(Size(100, 80)),
-        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-          RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0), // Border radius of 10.0
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(color),
+          minimumSize: MaterialStateProperty.all<Size>(Size(100, 80)),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius:
+                  BorderRadius.circular(10.0), // Border radius of 10.0
+            ),
           ),
         ),
-      ),
-      onPressed: () {},
-      child: Text(
-        label,
-        style: TextStyle(
-          fontSize: 40,
-          color: Colors.white,
-          fontWeight: FontWeight.bold,
+        onPressed: () {},
+        child: Text(
+          label,
+          style: TextStyle(
+            fontSize: 40,
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
     );
