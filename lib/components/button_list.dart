cf0409177d67e0390bfa7205f8e554a5b1b8b523
Luca <73708474+Luca-Landri@users.noreply.github.com>
2023-05-13 20:34:49 +0200

    Feat: Added possibility to add function

--- lib/components/button_list.dart
+++ lib/components/button_list.dart
@@ -7,11 +7,13 @@ class ButtonList extends StatefulWidget {
   final List<Color> colorArray;
   final List<Size> dimensionsArray;
   final List<dynamic> labelArray;
+  final List<Function> onPressedArray;
 
   const ButtonList({
     required this.colorArray,
     required this.dimensionsArray,
     required this.labelArray,
+    required this.onPressedArray,
   });
 
   @override
@@ -22,6 +24,7 @@ class _ButtonListState extends State<ButtonList> {
   late List<Color> colorArray = widget.colorArray;
   late List<Size> dimensionsArray = widget.dimensionsArray;
   late List<dynamic> labelArray = widget.labelArray;
+  late List<Function> onPressedArray = widget.onPressedArray;
 
   @override
   Widget build(BuildContext context) {
@@ -36,7 +39,7 @@ class _ButtonListState extends State<ButtonList> {
           dimensions: dimensionsArray[index],
           onPressed: () {
             // ignore: avoid_print
-            print("Button $index pressed");
+            onPressedArray[index]();
           },
         );
       }).toList(),
