20e2af94ce4d27d775e4cfeb1d51df5595f53db6
Luca <73708474+Luca-Landri@users.noreply.github.com>
2023-05-13 19:20:57 +0200

    Fix: Button grid

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,4 @@
-import 'package:calcolandri/components/Button_List.dart';
+import 'package:calcolandri/components/button_list.dart';
 import 'package:flutter/material.dart';
 
 class MyApp extends StatefulWidget {
@@ -9,6 +9,13 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  // ignore: prefer_const_constructors
+  final TextStyle style = TextStyle(
+    color: Colors.white,
+    fontSize: 30,
+    fontWeight: FontWeight.bold,
+  );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -22,9 +29,8 @@ class _MyAppState extends State<MyApp> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               // ignore: prefer_const_constructors
-              Placeholder(
-                fallbackHeight: 200,
-                fallbackWidth: 100,
+              SizedBox(
+                child: Text("Ciao"),
               ),
               // ignore: prefer_const_constructors
               Padding(
@@ -33,60 +39,110 @@ class _MyAppState extends State<MyApp> {
                   mainAxisAlignment: MainAxisAlignment.center,
                   // ignore: prefer_const_literals_to_create_immutables
                   children: [
-                    buttonList(
+                    ButtonList(
+                      // ignore: prefer_const_literals_to_create_immutables
+                      colorArray: [
+                        Color(0xFF616161),
+                        Colors.black45,
+                        Colors.black45,
+                        Colors.black45,
+                        Colors.black45,
+                      ],
+                      // ignore: prefer_const_literals_to_create_immutables
+                      dimensionsArray: [
+                        const Size(100, 80),
+                        const Size(100, 80),
+                        const Size(100, 80),
+                        const Size(100, 80),
+                        const Size(100, 80),
+                      ],
+
+                      // ignore: prefer_const_literals_to_create_immutables
+                      labelArray: [
+                        Text("Ac", style: style),
+                        Text("7", style: style),
+                        Text("4", style: style),
+                        Text("1", style: style),
+                        Text("Sin", style: style),
+                      ],
+                    ),
+                    // ignore: prefer_const_constructors
+                    ButtonList(
+                      // ignore: prefer_const_literals_to_create_immutables
+                      colorArray: [
+                        Color(0xFF616161),
+                        Colors.black45,
+                        Colors.black45,
+                        Colors.black45,
+                        Colors.black45,
+                      ],
+                      // ignore: prefer_const_literals_to_create_immutables
+                      dimensionsArray: [
+                        const Size(100, 80),
+                        const Size(100, 80),
+                        const Size(100, 80),
+                        const Size(100, 80),
+                        const Size(100, 80),
+                      ],
+
+                      // ignore: prefer_const_literals_to_create_immutables
+                      labelArray: [
+                        Icon(Icons.backspace, color: Colors.white, size: 30),
+                        Text("8", style: style),
+                        Text("5", style: style),
+                        Text("2", style: style),
+                        Text("0", style: style),
+                      ],
+                    ),
+                    ButtonList(
+                      // ignore: prefer_const_literals_to_create_immutables
                       colorArray: [
                         Color.fromRGBO(58, 53, 121, 0.8),
-                        Colors.blueGrey,
-                        Colors.blueGrey,
-                        Colors.blueGrey,
+                        Colors.black45,
+                        Colors.black45,
+                        Colors.black45,
+                        Colors.black45,
                       ],
+                      // ignore: prefer_const_literals_to_create_immutables
                       dimensionsArray: [
                         const Size(100, 80),
                         const Size(100, 80),
                         const Size(100, 80),
                         const Size(100, 80),
+                        const Size(100, 80),
+                      ],
+
+                      // ignore: prefer_const_literals_to_create_immutables
+                      labelArray: [
+                        Text("/", style: style),
+                        Text("9", style: style),
+                        Text("6", style: style),
+                        Text("3", style: style),
+                        Text(".", style: style),
                       ],
                     ),
-                    buttonList(
+                    ButtonList(
+                      // ignore: prefer_const_literals_to_create_immutables
                       colorArray: [
-                        Colors.blueGrey,
-                        Colors.blueGrey,
-                        Colors.blueGrey,
-                        Colors.blueGrey,
+                        Color.fromRGBO(58, 53, 121, 0.8),
+                        Color.fromRGBO(58, 53, 121, 0.8),
+                        Color.fromRGBO(58, 53, 121, 0.8),
+                        Color.fromRGBO(58, 53, 121, 0.8),
                       ],
+                      // ignore: prefer_const_literals_to_create_immutables
                       dimensionsArray: [
                         const Size(100, 80),
                         const Size(100, 80),
-                        const Size(100, 80),
-                        const Size(100, 80),
-                      ],
-                    ),
-                    buttonList(
-                      colorArray: [
-                        Colors.blueGrey,
-                        Colors.blueGrey,
-                        Colors.blueGrey,
-                        Colors.blueGrey,
-                      ],
-                      dimensionsArray: [
-                        const Size(100, 80),
-                        const Size(100, 80),
-                        const Size(100, 80),
+                        const Size(100, 167),
                         const Size(100, 80),
                       ],
-                    ),
-                    buttonList(
-                      colorArray: [
-                        Colors.blueGrey,
-                        Colors.blueGrey,
-                        Colors.blueGrey,
-                        Colors.blueGrey,
-                      ],
-                      dimensionsArray: [
-                        const Size(80, 80),
-                        const Size(80, 80),
-                        const Size(80, 80),
-                        const Size(80, 80),
+
+                      // ignore: prefer_const_literals_to_create_immutables
+                      labelArray: [
+                        Text("*", style: style),
+                        Text("-", style: style),
+                        Text("+", style: style),
+                        Text("=", style: style),
                       ],
                     ),
                   ],
